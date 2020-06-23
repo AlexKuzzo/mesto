5db1b8592f90bfe4a465113abc71af64818c79ff
Alexey Kuznetsov <kuzzo23@yandex.ru>
2020-06-23 21:57:10 +0300

    fix: исправить ошибки указанные ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,9 @@
 const popup = document.querySelector('.popup');
-const popupOverlay = popup.querySelector('.popup__overlay');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const popupOpenButton = document.querySelector('.profile__edit-button');
-
-
 const popupForm = document.querySelector('.popup__form');
 const profileName = document.querySelector('.profile__title');
-const profileDescription = document.querySelector('.profile__subtitle');
+const profileJob = document.querySelector('.profile__subtitle');
 
 let nameInput = document.querySelector('.popup__field_type_name');
 let jobInput = document.querySelector('.popup__field_type_description');
@@ -17,21 +14,19 @@ const popupToggle = function () {
   
   if (popup.classList.contains('popup_opened')) {
     nameInput.value = profileName.textContent;
-    jobInput.value = profileDescription.textContent;
+    jobInput.value = profileJob.textContent;
   }
 }
-popupToggle();
+
 
   popupOpenButton.addEventListener('click', popupToggle);
   popupCloseButton.addEventListener('click', popupToggle);
-  popupOverlay.addEventListener('click', popupToggle);
 
 function formSubmitHandler(event) {
     event.preventDefault();
     profileName.textContent = nameInput.value;
-    profileDescription.textContent = jobInput.value;
-    popupToggle();
-    
+    profileJob.textContent = jobInput.value;
+    popupToggle(); 
 }
     
     popupForm.addEventListener('submit', formSubmitHandler);
