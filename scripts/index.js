2f626a736d60b4eb3b996547ee0b89705b7b5365
Alexey Kuznetsov <kuzzo23@yandex.ru>
2020-07-07 18:04:57 +0300

    feat: добавить template, fix: js code popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,48 @@
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const popup = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup_type_profile');
+const popupAddCard = document.querySelector('.popup_type_add-card');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupForm = document.querySelector('.popup__form');
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__subtitle');
+const popupAddButton = document.querySelector('.profile__add-button');
 
 let nameInput = document.querySelector('.popup__field_type_name');
 let jobInput = document.querySelector('.popup__field_type_description');
 
 
 const popupToggle = function () {
-  popup.classList.toggle('popup_opened')
+  popup.classList.toggle('popup_opened');
   
-  if (popup.classList.contains('popup_opened')) {
+  if (popupProfile.classList.contains('popup_opened')) {
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
   }
@@ -22,14 +52,25 @@ const popupToggle = function () {
   popupOpenButton.addEventListener('click', popupToggle);
   popupCloseButton.addEventListener('click', popupToggle);
 
-function formSubmitHandler(event) {
-    event.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    popupToggle(); 
+const formSubmitHandler = function(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popupToggle(popupProfile); 
 }
     
-    popupForm.addEventListener('submit', formSubmitHandler);
+  popupForm.addEventListener('submit', formSubmitHandler);
+
+// popupAddCard
+const placeSubmitHandler = function(evt) {
+  evt.preventDefault();
+
+  popupToggle(popupAddCard);
+}
+
+popupAddButton.addEventListener('click', popupToggle);
+
+
     
     
 
